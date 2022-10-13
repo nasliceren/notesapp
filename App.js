9898737ec9a5e7011602d41f69071808ed10a77f
nazlikoc <n.ceren120@hotmail.com>
2022-10-13 13:03:43 +0200

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,63 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    addButtonWrapper: {
+        display: 'flex',
+        position: 'absolute',
+        bottom: 50,
+        width: 50,
+        height: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    addButton: {
+        fontSize: '70px',
+        color: '#5a2a81',
+        top: -25,
+        height: 100,
+    },
+    addButtonCircle: {
+        display: 'flex',
+        alignItems: '',
+        borderWidth: 3,
+        borderStyle: 'solid',
+        borderRadius: '100',
+        borderColor: '#5a2a81',
+        height: 50,
+        width: 50,
+    },
+    caption: {
+        fontSize: '20px',
+    },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+        paddingTop: 60,
+    },
+    horizontalLine: {
+        marginTop: 10,
+        borderColor: 'gray',
+        borderWidth: 1,
+        width: '70%',
+    },
 });
+
+export default function App() {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.caption}>Super Notes App</Text>
+            <View style={styles.horizontalLine} />
+            <StatusBar style="auto"/>
+            <View style={styles.addButtonWrapper}>
+                <TouchableOpacity style={styles.addButtonCircle}>
+                    <Text style={styles.addButton}>+</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
+}
+
+
